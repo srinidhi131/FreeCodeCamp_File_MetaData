270d58da4cdeb8536189500f233df9467306d8c4
srinidhi131 <66429963+srinidhi131@users.noreply.github.com>
2021-06-02 22:16:23 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var cors = require('cors');
+var multer = require('multer');
+var upload = multer({dest:'uploads/'});
+
+require('dotenv').config()
+
+var app = express();
+
+app.use(cors());
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  try {
+    console.log(req.file)
+    res.json({
+      "name":req.file.originalname,
+      "type":req.file.mimetype,
+      "size":req.file.size
+    })
+  }catch(err) {
+    res.send(400);
+  }
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
